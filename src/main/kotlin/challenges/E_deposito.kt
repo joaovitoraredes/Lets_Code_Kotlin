818c9a5c43ebe444476a93cb6877c43a32ae80f4
João Vitor <joaovitoraredes.jvv@gmail.com>
2022-11-25 19:22:28 -0300

    Finalizando desafios adicionando algumas funções com retorno de texto por conta de possível erros na entrada dos dados

--- src/main/kotlin/challenges/E_deposito.kt
+++ src/main/kotlin/challenges/E_deposito.kt
@@ -16,35 +16,36 @@ val menuPrincipal = """
 
 const val ADICIONAR_ITEM = 1
 const val EDITAR_ITEM = 2
-const val EXIBIR_ITENS_EM_ESTOQUE = 3
-const val EXIBIR_TODOS_OS_ITENS = 4
-const val FECHAR_O_SISTEMA = 0
+const val EXIBIR_TODOS_OS_ITENS = 3
+const val EXIBIR_ITENS_EM_ESTOQUE = 4
+const val FINALIZAR_O_PROGRAMA = 0
 
-val deposito = mutableListOf<Peca>()
+val deposito = mutableListOf <Item>()
 
 var id = 1
 
 fun main() {
-
     println("$bemVindo\n$menuPrincipal")
+    eDeposito()
+}
 
+fun eDeposito(){
     do {
 
         val selecionarOpcao = readln().toInt()
 
         when (selecionarOpcao) {
-            1 -> adicionarItem(selecionarOpcao)
-            2 -> editarItem()
-            3 -> exibirListaCompleta()
-            4 -> exibirListaEmEstoque()
-            else -> Unit
+            ADICIONAR_ITEM -> adicionarItem()
+            EDITAR_ITEM -> editarItem()
+            EXIBIR_TODOS_OS_ITENS -> exibirListaCompleta()
+            EXIBIR_ITENS_EM_ESTOQUE -> exibirListaEmEstoque()
+            FINALIZAR_O_PROGRAMA -> println("Finalizando programa...")
+            else -> println("Entrada inválida, entre com um dos valores abaixo:\n$menuPrincipal")
         }
     } while (selecionarOpcao != 0)
-
 }
 
-
-fun adicionarItem(continuar:Int) {
+fun adicionarItem() {
     val qtsP: Int
 
     println("Qual peça você deseja adicionar?")
@@ -54,27 +55,29 @@ fun adicionarItem(continuar:Int) {
     try {
          qtsP = validacaoDaQuantidade()
     } catch (e: LimitExceededException){
+        mensagemErroAdiconar()
         voltarMenuErro()
         return
     }
 
-    val peca = Peca(id,nomeP,qtsP)
+    val peca = Item(id,nomeP,qtsP)
     deposito.add(peca)
     id+=1
 
+    mensagemSucessoAdiconar()
     voltarMenuSucesso()
 }
+fun editarItem(depositoTotal: List <Item> = deposito){
 
-fun editarItem(depositoTotal: List<Peca> = deposito){
-
-    exibirListaCompleta()
+    apenasMostrarLista()
 
     println("Qual item você deseja editar?")
     val idParaEditar = readln().toInt()
 
     for (peca in depositoTotal) {
+
         if (idParaEditar == peca.id) {
-            println("#%04d".format(peca.id) + " | ${peca.nome} | ${peca.qts}")
+            println("#%04d".format(peca.id) + " | ${peca.nome} | ${peca.quantitadeP}")
 
             println("E oque você deseja editar? (nome/quantidade)")
             when (readln().lowercase()) {
@@ -82,70 +85,133 @@ fun editarItem(depositoTotal: List<Peca> = deposito){
                     println("Qual o novo nome? ")
                     peca.nome = readln()
                     println("Nome alterado com sucesso")
+                    mensagemSucessoEditar()
                     voltarMenuSucessoEdicao()
                 }
 
                 "quantidade" -> {
                     println("Qual a nova quantidade? ")
                     try {
-                        peca.qts = validacaoDaQuantidade()
+                        peca.quantitadeP = validacaoDaQuantidade()
                     }   catch (e:LimitExceededException){
+                        mensagemErroEditar()
                         voltarMenuErroEdicao()
                         return
                     }
+                    mensagemSucessoEditar()
                     voltarMenuSucessoEdicao()
                 }
             }
         }
     }
 }
-
-fun exibirListaCompleta(pecasExibicao: List<Peca> = deposito) {
+fun exibirListaCompleta(pecasExibicao: List <Item> = deposito) {
     println("ID | Peça | Quantidade")
     for (peca in pecasExibicao) {
-        println("#%04d".format(peca.id) + " | ${peca.nome} | ${peca.qts}")
+        println("#%04d".format(peca.id) + " | ${peca.nome} | ${peca.quantitadeP}")
     }
+    voltarMenuListaC()
 }
-
-fun exibirListaEmEstoque(pecasExibicao: List<Peca> = deposito) {
+fun exibirListaEmEstoque(pecasExibicao: List <Item> = deposito) {
     println("ID | Peça | Quantidade")
     for (peca in pecasExibicao) {
-        if (peca.qts > 0)
-        println("#%04d".format(peca.id) + " | ${peca.nome} | ${peca.qts}")
+        if (peca.quantitadeP > 0)
+        println("#%04d".format(peca.id) + " | ${peca.nome} | ${peca.quantitadeP}")
     }
-
-    println("Voltar ao menu principal? (S/N)")
-    if (readln().uppercase()=="S") println(menuPrincipal)
-
+    voltarMenuListaE()
 }
-
 fun validacaoDaQuantidade():Int{
-    val quantidade = readln().toInt()
-    if (quantidade > 999 || quantidade < 0){
+    val quantidade = try {
+        readln().toInt()
+    } catch (e: NumberFormatException)
+    { null }
+    if (quantidade == null || quantidade > 999 || quantidade < 0){
         throw LimitExceededException(null)
     }
     return quantidade
 }
-
-fun voltarMenuErro(){
+fun apenasMostrarLista(pecasExibicao: List <Item> = deposito) {
+    println("ID | Peça | Quantidade")
+    for (peca in pecasExibicao) {
+        println("#%04d".format(peca.id) + " | ${peca.nome} | ${peca.quantitadeP}")
+    }
+}
+//Mensagens de retorno
+fun mensagemErroAdiconar(){
     println("Item não adicionado - Quantidade inválida!! \n" +
             "Voltar ao menu principal? (S/N)")
-    if (readln().uppercase()=="S") println(menuPrincipal)
 }
-fun voltarMenuErroEdicao(){
+fun mensagemErroEditar(){
     println("Item não editado - Quantidade inválida!! \n" +
             "Voltar ao menu principal? (S/N)")
-    if (readln().uppercase()=="S") println(menuPrincipal)
 }
-
+fun mensagemSucessoAdiconar(){
+    println("Item adicionado!! \n" + "Voltar ao menu principal? (S/N)")
+}
+fun mensagemSucessoEditar(){
+    println("Item editado!! \n" + "Voltar ao menu principal? (S/N)")
+}
+fun voltarMenuListaC(){
+    println("Voltar ao menu principal? (S/N)")
+    when(readln().uppercase()){
+        "S" -> println(menuPrincipal)
+        "N" -> exibirListaCompleta()
+        else -> {
+            println("Entrada inválida!! \nVoltar ao menu principal? (S/N)")
+            voltarMenuListaC()
+        }
+    }
+}
+fun voltarMenuListaE(){
+    println("Voltar ao menu principal? (S/N)")
+    when(readln().uppercase()){
+        "S" -> println(menuPrincipal)
+        "N" -> exibirListaEmEstoque()
+        else -> {
+            println("Entrada inválida!! \nVoltar ao menu principal? (S/N)")
+            voltarMenuListaE()
+        }
+    }
+}
+fun voltarMenuErro(){
+    when(readln().uppercase()){
+        "S" -> println(menuPrincipal)
+        "N" -> adicionarItem()
+        else -> {
+            println("Entrada inválida!! \nVoltar ao menu principal? (S/N)")
+            voltarMenuErro()
+        }
+    }
+}
+fun voltarMenuErroEdicao(){
+    when(readln().uppercase()){
+        "S" -> println(menuPrincipal)
+        "N" -> editarItem()
+        else -> {
+            println("Entrada inválida!! \nVoltar ao menu principal? (S/N)")
+            voltarMenuErroEdicao()
+        }
+    }
+}
 fun voltarMenuSucesso(){
-    println("Item Adicionado!! \nVoltar ao menu principal? (S/N)")
-    if (readln().uppercase()=="S") println(menuPrincipal)
+    when(readln().uppercase()){
+        "S" -> println(menuPrincipal)
+        "N" -> adicionarItem()
+        else -> {
+            println("Entrada inválida!! \nVoltar ao menu principal? (S/N)")
+            voltarMenuErro()
+        }
+    }
 }
 fun voltarMenuSucessoEdicao(){
-    println("Item editado!! \nVoltar ao menu principal? (S/N)")
-    if (readln().uppercase()=="S") println(menuPrincipal)
-}
+    when(readln().uppercase()){
+        "S" -> println(menuPrincipal)
+        "N" -> editarItem()
+        else -> {
+            println("Entrada inválida!! \nVoltar ao menu principal? (S/N)")
+            voltarMenuErro()
+        }
+    }}
 
-class Peca(val id: Int, var nome:String, var qts: Int){}
+class Item(val id: Int, var nome:String, var quantitadeP: Int){}
 
