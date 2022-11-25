1b20ec2d4fdcb5d19d5dafdca7be700b255246be
João Vitor <joaovitoraredes.jvv@gmail.com>
2022-11-25 15:23:08 -0300

    adicionado função editarItem/exibirListaCompleta/exibirListaEmEstoque/validaçãoDeQuantidade/e funções para exibição de mensagem de sucesso e erro

--- src/main/kotlin/challenges/E_deposito.kt
+++ src/main/kotlin/challenges/E_deposito.kt
@@ -1,11 +1,16 @@
 package challenges
 
+import javax.naming.LimitExceededException
+
+val bemVindo = """
+    ========= Bem Vindo ==========
+    """.trimIndent()
+
 val menuPrincipal = """
-        ========= Bem Vindo ==========
         1...............ADICIONAR ITEM
         2..................EDITAR ITEM
-        3......EXIBIR ITENS EM ESTOQUE
-        4........EXIBIR TODOS OS ITENS
+        3........EXIBIR TODOS OS ITENS
+        4......EXIBIR ITENS EM ESTOQUE
         0.............FECHAR O SISTEMA
     """.trimIndent()
 
@@ -15,60 +20,132 @@ const val EXIBIR_ITENS_EM_ESTOQUE = 3
 const val EXIBIR_TODOS_OS_ITENS = 4
 const val FECHAR_O_SISTEMA = 0
 
-val deposito: MutableList<String> = mutableListOf()
+val deposito = mutableListOf<Peca>()
 
+var id = 1
 
 fun main() {
-    println(menuPrincipal)
+
+    println("$bemVindo\n$menuPrincipal")
 
     do {
 
         val selecionarOpcao = readln().toInt()
 
         when (selecionarOpcao) {
-            1 -> adicionarItem()
-//            2 -> editarItem()
+            1 -> adicionarItem(selecionarOpcao)
+            2 -> editarItem()
+            3 -> exibirListaCompleta()
+            4 -> exibirListaEmEstoque()
+            else -> Unit
         }
-
     } while (selecionarOpcao != 0)
 
-    mostrarComanda()
 }
 
-fun linhaComanda(
-    id: Int,
-    peca: String,
-    qts:Int
-): String = "#$id | $peca | $qts"
 
-fun adicionarItem() {
+fun adicionarItem(continuar:Int) {
+    val qtsP: Int
+
     println("Qual peça você deseja adicionar?")
-    val pecaEntradaUsuario = readln()
+    val nomeP = readln()
 
     println("Qual a quantidade?")
-    val qtsPecasEntradaUsuario = readln().toInt()
+    try {
+         qtsP = validacaoDaQuantidade()
+    } catch (e: LimitExceededException){
+        voltarMenuErro()
+        return
+    }
 
-    val linhaItem = linhaComanda(id = deposito.size.inc(),  peca = pecaEntradaUsuario, qts = qtsPecasEntradaUsuario )
-    deposito.add(linhaItem)
+    val peca = Peca(id,nomeP,qtsP)
+    deposito.add(peca)
+    id+=1
+
+    voltarMenuSucesso()
 }
 
-fun mostrarComanda(){
-    deposito.forEach { linhaItem ->
-        println(linhaItem)
+fun editarItem(depositoTotal: List<Peca> = deposito){
+
+    exibirListaCompleta()
+
+    println("Qual item você deseja editar?")
+    val idParaEditar = readln().toInt()
+
+    for (peca in depositoTotal) {
+        if (idParaEditar == peca.id) {
+            println("#%04d".format(peca.id) + " | ${peca.nome} | ${peca.qts}")
+
+            println("E oque você deseja editar? (nome/quantidade)")
+            when (readln().lowercase()) {
+                "nome" -> {
+                    println("Qual o novo nome? ")
+                    peca.nome = readln()
+                    println("Nome alterado com sucesso")
+                    voltarMenuSucessoEdicao()
+                }
+
+                "quantidade" -> {
+                    println("Qual a nova quantidade? ")
+                    try {
+                        peca.qts = validacaoDaQuantidade()
+                    }   catch (e:LimitExceededException){
+                        voltarMenuErroEdicao()
+                        return
+                    }
+                    voltarMenuSucessoEdicao()
+                }
+            }
+        }
     }
 }
 
+fun exibirListaCompleta(pecasExibicao: List<Peca> = deposito) {
+    println("ID | Peça | Quantidade")
+    for (peca in pecasExibicao) {
+        println("#%04d".format(peca.id) + " | ${peca.nome} | ${peca.qts}")
+    }
+}
 
+fun exibirListaEmEstoque(pecasExibicao: List<Peca> = deposito) {
+    println("ID | Peça | Quantidade")
+    for (peca in pecasExibicao) {
+        if (peca.qts > 0)
+        println("#%04d".format(peca.id) + " | ${peca.nome} | ${peca.qts}")
+    }
 
+    println("Voltar ao menu principal? (S/N)")
+    if (readln().uppercase()=="S") println(menuPrincipal)
 
+}
 
+fun validacaoDaQuantidade():Int{
+    val quantidade = readln().toInt()
+    if (quantidade > 999 || quantidade < 0){
+        throw LimitExceededException(null)
+    }
+    return quantidade
+}
 
+fun voltarMenuErro(){
+    println("Item não adicionado - Quantidade inválida!! \n" +
+            "Voltar ao menu principal? (S/N)")
+    if (readln().uppercase()=="S") println(menuPrincipal)
+}
+fun voltarMenuErroEdicao(){
+    println("Item não editado - Quantidade inválida!! \n" +
+            "Voltar ao menu principal? (S/N)")
+    if (readln().uppercase()=="S") println(menuPrincipal)
+}
 
+fun voltarMenuSucesso(){
+    println("Item Adicionado!! \nVoltar ao menu principal? (S/N)")
+    if (readln().uppercase()=="S") println(menuPrincipal)
+}
+fun voltarMenuSucessoEdicao(){
+    println("Item editado!! \nVoltar ao menu principal? (S/N)")
+    if (readln().uppercase()=="S") println(menuPrincipal)
+}
 
+class Peca(val id: Int, var nome:String, var qts: Int){}
 
-
-
-
-//val comandaFinalFormato = """
-//        ID | PEÇA | QUANTIDADE
-//        """.trimIndent()
